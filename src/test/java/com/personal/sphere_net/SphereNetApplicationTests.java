189cbc9dd6bc5d51002cbe5d61c7ab120a74590e
JENO JIJI <jenojiji001@gmail.com>
2024-11-15 18:05:49 +0530

    chore:init

---
+++ src/test/java/com/personal/sphere_net/SphereNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.sphere_net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SphereNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

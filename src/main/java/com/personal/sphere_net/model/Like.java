5e9b7ad817247ab3e4ce764ed62d43e4730eefc3
JENO JIJI <jenojiji001@gmail.com>
2024-11-19 18:27:39 +0530

    like ,dislike done

--- src/main/java/com/personal/sphere_net/model/Like.java
+++ src/main/java/com/personal/sphere_net/model/Like.java
@@ -37,4 +37,13 @@ public class Like {
     protected void onCreate() {
         created_at = LocalDateTime.now();
     }
+
+    @PrePersist
+    @PreUpdate
+    private void validateAssociation() {
+        if ((post == null && comment == null) || (post != null && comment != null)) {
+            throw new IllegalStateException("A Like must be associated with either a Post or a Comment, but not both.");
+        }
+    }
+
 }
---
+++ src/main/java/com/personal/sphere_net/repository/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.personal.sphere_net.repository;
+
+import com.personal.sphere_net.model.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like,Long> {
+
+}
